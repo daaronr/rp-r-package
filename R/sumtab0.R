d50a07f615f6252fe8c8682e9cf23cd542b888e0
@daaronr <8229168+daaronr@users.noreply.github.com>
2023-04-26 17:45:54 -0400

    some new versions of sumtab

---
+++ R/sumtab0.R
@@ -0,0 +1,25 @@
+#' sumtab but without kable and adding a few rows
+#'
+#' @examples
+#'  sumtab0(movies_long, budget, genre)
+
+#' @export
+
+sumtab0 <- function(df, depvar, treatvar, caption = "", digits=3, label = TRUE) {
+  require(dplyr, kableExtra)
+  df %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na({{depvar}})) %>%
+    group_by({{treatvar}}) %>%
+    dplyr::summarize(N = n(),
+                     `share > 0` = sum({{depvar}} >0)/n(),
+                     `share missing` = sum(is.na({{depvar}})) / n(),
+                     #share_10 = sum({{depvar}}== 10)/n(),
+                     Mean = round(mean({{depvar}}, na.rm = T), 2),
+                     Median = round(median({{depvar}}, na.rm = T),2),
+                     P80 = round(quantile({{depvar}}, 0.8, na.rm = T), 2),
+                     P99 = round(quantile({{depvar}}, 0.99, na.rm = T), 2),
+                     Std.dev. = glue::glue("(", {round(sd({{depvar}}, na.rm = T), 2) }, ")"))
+   # kable(caption = caption, digits=digits, label = label) %>%
+    #kable_styling()
+}
--- R/sumtab_se.R
+++ R/sumtab_se.R
@@ -28,3 +28,20 @@ sumtab_se <- function(df, depvar, treatvar, caption = "", digits=3, label = TRUE
     kable(caption = caption, digits=digits, label = label) %>%
     kable_styling()
 }
+
+
+sumtab_se_no_kable <- function(df, depvar, treatvar, caption = "", digits=3, label = TRUE) {
+  require(dplyr, kableExtra)
+  df %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!is.na({{depvar}})) %>%
+    group_by({{treatvar}}) %>%
+    dplyr::summarize(N = n(),
+                     `share > 0` = sum({{depvar}} >0)/n(),
+                     #share_10 = sum({{depvar}}== 10)/n(),
+                     Mean = round(mean({{depvar}}, na.rm = T), 2),
+                     SE = sd({{depvar}}, na.rm=T)/sqrt(sum(!is.na({{depvar}}))),
+                     Median = round(median({{depvar}}, na.rm = T),2),
+                     P80 = round(quantile({{depvar}}, 0.8, na.rm = T), 2),
+                     # P99 = round(quantile({{depvar}}, 0.99, na.rm = T), 2),
+                     Std.dev. = glue::glue("(", {round(sd({{depvar}}, na.rm = T), 2) }, ")"))}
