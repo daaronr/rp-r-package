4c3131adf70f7bb8cf47ab3296cd1ce96a2c74a4
@daaronr <8229168+daaronr@users.noreply.github.com>
2023-05-05 13:12:10 -0400

    Update sumtab0.R

--- R/sumtab0.R
+++ R/sumtab0.R
@@ -11,8 +11,8 @@ sumtab0 <- function(df, depvar, treatvar, caption = "", digits=3, label = TRUE) 
     dplyr::ungroup() %>%
     group_by({{treatvar}}) %>%
     dplyr::summarize(N = n(),
-                     `share > 0` = sum({{depvar}} >0)/n(),
-                     `share missing` = sum(is.na({{depvar}})) / n(),
+                     `share > 0` = round(sum({{depvar}} >0)/n(), 2),
+                     `share missing` = round(sum(is.na({{depvar}})) / n(), 2),
                      #share_10 = sum({{depvar}}== 10)/n(),
                      Mean = round(mean({{depvar}}, na.rm = T), 2),
                      Median = round(median({{depvar}}, na.rm = T),2),
